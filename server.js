f6280e4646a54c7c7c6ee1b8e896601e46631719
Glitch (slack-slash-command-test) <none>
2020-05-23 13:36:42 +0000

    💑🎢 Update response with new payload builder

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ const in_channel = msg => {
 var msg_unit = msg => {
   var unit = {
     text: msg,
-    _add: msg => {
-      unit.text += "" + msg;
+    _add: more => {
+      unit.text += "" + more;
       return unit;
     },
     _set: (k, v) => {
@@ -50,7 +50,7 @@ var msg_unit = msg => {
     _end: () => {
       delete unit._set;
       delete unit._end;
-      return JSON.stringify(unit);
+      return unit;
     }
   };
   return unit;
@@ -71,7 +71,23 @@ app.get("/", (request, response) => {
 
 app.post("/", (req, res) => {
   // res.json(in_channel("POST request: " + req.get("Content-Type")));
-  res.json(in_channel("POST request: " + JSON.stringify(req.body)));
+  // res.json(in_channel("POST request: " + JSON.stringify(req.body)));
+  res.json(
+    msg_unit("POST request")
+      ._set("response_type", "in_channel")
+      ._add("\n" + "text: " + req.body.text)
+      ._add("\n" + "token: " + req.body.token)
+      ._add("\n" + "team_id: " + req.body.team_id)
+      ._add("\n" + "team_domain: " + req.body.team_domain)
+      ._add("\n" + "channel_id: " + req.body.channel_id)
+      ._add("\n" + "channel_name: " + req.body.channel_name)
+      ._add("\n" + "user_id: " + req.body.user_id)
+      ._add("\n" + "user_name: " + req.body.user_name)
+      ._add("\n" + "command: " + req.body.command)
+      ._add("\n" + "text: " + req.body.text)
+      ._add("\n" + "response_url: " + req.body.response_url)
+      ._end()
+  );
 });
 
 // listen for requests :)
